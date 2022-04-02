1c12d9aff7c8c4122127a92c9088d6a63094d6d3
Viktoria <zaharova181114@gmail.com>
2022-04-02 23:31:01 +0300

    add block

--- app/js/common.js
+++ app/js/common.js
@@ -13,3 +13,28 @@ $(window).on('load resize', function() {
     }
 });
 // slick active
+
+$('.btn-burger').on('click', function () {
+   $('.mobile-menu').fadeToggle();
+});
+
+$('.btn-close').on('click', function () {
+    $('.mobile-menu').fadeOut();
+});
+
+//плавный скролл
+$(document).ready(function () {
+    $('.go_to').click(function (e) {
+        e.preventDefault();
+        var scroll_el = $(this).attr('href');
+        if ($(scroll_el).length != 0) {
+            $('html, body').animate({
+                scrollTop: $(scroll_el).offset().top
+            }, 500);
+        }
+        return false;
+    });
+});
+//плавный скролл end
+
+$('[name="phone"]').mask('+7 (999) 999-99-99');
