f69a04c1a431796c88154f886fdbaf46cae7d3fd
Viktoria <zaharova181114@gmail.com>
2022-04-04 20:18:38 +0300

    edit1

--- app/js/common.js
+++ app/js/common.js
@@ -27,14 +27,47 @@ $(document).ready(function () {
     $('.go_to').click(function (e) {
         e.preventDefault();
         var scroll_el = $(this).attr('href');
-        if ($(scroll_el).length != 0) {
+        if ($(scroll_el).length !== 0) {
             $('html, body').animate({
                 scrollTop: $(scroll_el).offset().top
             }, 500);
         }
+        $('.mobile-menu').fadeOut();
+
         return false;
     });
 });
 //плавный скролл end
 
 $('[name="phone"]').mask('+7 (999) 999-99-99');
+
+$(document).ready(function () {
+
+    $(".form").submit(function () {
+        $.ajax({
+            type: "POST",
+            url: "mail.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            // alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+
+            $('.modal').removeClass('show');
+
+            $('#thanksModal').addClass('show');
+
+            $(".form").trigger("reset");
+        });
+        return false;
+    });
+
+});
+
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 150) {
+        $('header').addClass('fixed');
+    } else {
+        $('header').removeClass('fixed');
+    }
+});
